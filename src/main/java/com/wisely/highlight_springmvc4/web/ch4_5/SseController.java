28a1153047fd780067587d490b3ddaef8660dbee
谢丹 <rmqc0909@163.com>
2017-01-11 17:43:21 +0800

    springboot第二部分  sse

--- src/main/java/com/wisely/highlight_springmvc4/web/ch4_5/SseController.java
+++ src/main/java/com/wisely/highlight_springmvc4/web/ch4_5/SseController.java
@@ -1,18 +1,17 @@
 package com.wisely.highlight_springmvc4.web.ch4_5;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
 /**
  * Created by xiedan11 on 2017/1/10.
  */
-@Controller
+@RestController
 public class SseController {
     @RequestMapping(value = "/push", produces = "text/event-stream")
-    public @ResponseBody String push() {
+    public String push() {
         try {
             Thread.sleep(20000);
         } catch (InterruptedException e) {
