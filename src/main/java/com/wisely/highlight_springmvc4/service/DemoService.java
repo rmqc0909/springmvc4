8993b0b5b1a5092c7c4c1163d3649471aee68668
谢丹 <rmqc0909@163.com>
2017-01-11 17:34:51 +0800

    springboot第二部分  测试部分

---
+++ src/main/java/com/wisely/highlight_springmvc4/service/DemoService.java
@@ -0,0 +1,13 @@
+package com.wisely.highlight_springmvc4.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by xiedan11 on 2017/1/11.
+ */
+@Service
+public class DemoService {
+    public String saySomething() {
+        return "hello";
+    }
+}
---
+++ src/test/java/com/wisely/highlight_springmvc4/web/ch4_6/TestControllerIntegrationTests.java
@@ -0,0 +1,8 @@
+package com.wisely.highlight_springmvc4.web.ch4_6;
+
+/**
+ * Created by xiedan11 on 2017/1/11.
+ */
+public class TestControllerIntegrationTests {
+
+}
