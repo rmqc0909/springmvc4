91459e3b9e35d330ebd1e22266d54cc75849635a
谢丹 <rmqc0909@163.com>
2017-01-12 11:37:49 +0800

    springboot第二部分  springmvc的测试

---
+++ src/main/resources/views/page.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: xiedan11
+  Date: 2017/1/12
+  Time: 11:07
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Test page</title>
+</head>
+<body>
+    <pre>
+        Welcome to Spring Mvc Test World!
+    </pre>
+</body>
+</html>
--- src/test/java/com/wisely/highlight_springmvc4/web/ch4_6/TestControllerIntegrationTests.java
+++ src/test/java/com/wisely/highlight_springmvc4/web/ch4_6/TestControllerIntegrationTests.java
@@ -1,8 +1,52 @@
 package com.wisely.highlight_springmvc4.web.ch4_6;
 
+import com.wisely.highlight_springmvc4.MyMvcConfig;
+import com.wisely.highlight_springmvc4.service.DemoService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 /**
  * Created by xiedan11 on 2017/1/11.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {MyMvcConfig.class})
+@WebAppConfiguration("src/main/resources")
 public class TestControllerIntegrationTests {
+    private MockMvc mockMvc;
+    @Autowired
+    private DemoService demoService;
+    @Autowired
+    WebApplicationContext webApplicationContext;
 
+    @Before     //在测试开始前的初始化工作
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    public void testNormalController() throws Exception{
+        mockMvc.perform(get("/normal"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("page"))
+                .andExpect(forwardedUrl("/WEB-INF/classes/views/page.jsp"))
+                .andExpect(model().attribute("msg", demoService.saySomething()));
+    }
+    @Test
+    public void testRestController() throws Exception {
+        mockMvc.perform(get("/testRest"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string(demoService.saySomething()));
+    }
 }
